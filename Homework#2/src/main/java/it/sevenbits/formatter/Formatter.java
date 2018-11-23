2fd86dbf2a0fdd6cc40c9cea44bfddc100a50da5
Maxim Kalashnik <kalashnikmaxim@mail.ru>
2018-11-23 18:11:17 +0600

    Trying to format

--- Homework#2/src/main/java/it/sevenbits/formatter/Formatter.java
+++ Homework#2/src/main/java/it/sevenbits/formatter/Formatter.java
@@ -1,20 +1,210 @@
 package it.sevenbits.formatter;
 
 public class Formatter {
-    private Context context;
     private StringBuilder result;
+    private char cur;
+    private char prev;
+    String alphabet;
+    private int tabCount;
+    boolean isNewLine;
 
-    public Formatter(Context context) {
-        this.context = context;
-        result = new StringBuilder();
+    public Formatter() {
+        this.result = new StringBuilder();
+        this.alphabet = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM.=(),\'\"+\\/<>[]";
+        this.tabCount = 0;
+        this.isNewLine = false;
     }
 
-    public void format(String input) {
-        for (int i = 0; i < input.length(); i++) {
-            if (!context.isInit()) {
-
+    public String format(String input) {
+        int len = input.length();
+        for (int i = 0; i < len; i++) {
+            if (i == 0) {
+                prev = input.charAt(i);
+            } else {
+                cur = input.charAt(i);
+                act2();
+                prev = cur;
             }
         }
+        act2();
+        return result.toString();
+    }
+//    public String format(String input) {
+//        int len = input.length();
+//        for (int i = 0; i < len; i++) {
+//            if (i == 0) {
+//                prev = input.charAt(i);
+//            } else {
+//                cur = input.charAt(i);
+//                act();
+//                prev = cur;
+//            }
+//            if (isNewLine) {
+//                addTab();
+//                isNewLine = false;
+//            }
+//        }
+//        act();
+//        return result.toString();
+//    }
 
+    private void action() {
+        if (isAlpha(prev) && isAlpha(cur)) {
+            result.append(prev);
+        }
+        else if (isAlpha(prev) && cur == ' ') {
+            result.append(prev);
+        }
+        else if (isAlpha(prev) && cur == ';') {
+            result.append(prev);
+        }
+        else if (prev == ' ' && isAlpha(cur)) {
+            result.append(prev);
+        }
+        else if (prev == ' ' && cur == ' ') {
+            ;
+        }
+        else if (prev == ' ' && cur == '{') {
+            result.append(prev);
+//            addTab();
+        }
+        else if (prev == ' ' && cur == '\n') {
+            ;
+        }
+        else if (prev == '{' && isAlpha(cur)) {
+            tabCount++;
+            result.append(prev).append('\n');
+            addTab();
+        }
+        else if (prev == '{' && (cur == ' ' || cur == '\n')) {
+            tabCount++;
+            result.append(prev).append('\n');
+            addTab();
+        }
+        else if (prev == '}' && cur == '\n') {
+            tabCount--;
+            addTab();
+            result.append(prev);
+        }
+        else if (prev == ';' && isAlpha(cur)) {
+            result.append(prev).append('\n');   // \n ?
+            addTab();
+        }
+        else if (prev == ';' && cur == ' ') {
+            result.append(prev);
+        }
+        else if (prev == ';' && cur == '\n') {
+            result.append(prev).append('\n');
+            addTab();
+        }
+//        else if (prev == '\n' && isAlpha(cur)) {
+//            result.append('\n');
+//            addTab();
+//        }
+        else if (prev == '\n' && cur == '}') {
+//            addTab();
+            result.append('\n');
+        }
+        else if (prev == '}') {
+            tabCount--;
+            addTab();
+            result.append(prev);
+        }
+    }
+
+//    private void act() {
+//        if (prev == ' ' && cur == ' ') {
+//            ; // pass
+//        }
+//        else if (prev == ' ' && cur == '}') {
+//            tabCount--;
+//            ; // pass
+//        }
+//        else if (isAlpha(prev) && cur == ' ') {
+//            result.append(prev).append(cur);
+//        }
+//
+//        else if (isAlpha(prev)) {
+//            result.append(prev);
+//        }
+//        else if (prev == '{') {
+//            result.append(prev);
+//            tabCount++;
+//        }
+//        else if (prev ==';' && cur != '\n') {
+//            result.append(prev);
+//        }
+//        else if (prev ==';') {
+//            result.append(prev);
+//        }
+//        else if (prev == '\n' && cur == '}') {
+//            tabCount--;
+////            result.setLength(result.length() - 1);  // delete tabulate(костыль)
+////            addTab();
+//            result.append(prev);
+//        }
+//        else if (prev == '\n' && cur == ' ') {
+//            result.append(prev);
+//            isNewLine = true;
+////            addTab();
+//        }
+//        else if (prev == '}') {
+////            tabCount--;
+//            result.append(prev);
+//        }
+//    }
+
+    private void act2() {
+        if (prev == ' ' && cur == ' ') {
+            ; // pass
+        }
+        else if (isNewLine && isAlpha(cur)) {
+            addTab();
+            isNewLine = false;
+        }
+        else if (isNewLine && cur == '}') {
+            tabCount--;
+            addTab();
+            isNewLine = false;
+        }
+        else if (isAlpha(prev) && cur == ' ') {
+            result.append(prev).append(cur);
+        }
+
+        else if (isAlpha(prev)) {
+            result.append(prev);
+        }
+        else if (prev == '{') {
+            result.append(prev);
+            tabCount++;
+        }
+        else if (prev ==';' && cur != '\n') {
+            result.append(prev);
+        }
+        else if (prev ==';') {
+            result.append(prev);
+        }
+        else if (prev == '\n' && cur == '}') {
+            isNewLine = true;
+            result.append(prev).append(cur);
+        }
+        else if (prev == '\n' && cur == ' ') {
+            isNewLine = true;
+            result.append(prev);
+        }
+        else if (prev == '}') {
+            result.append(prev);
+        }
+    }
+
+    private boolean isAlpha(char symb) {
+        String buff = "" + symb;
+        return alphabet.contains(buff);
+    }
+
+    private void addTab() {
+        for (int i = 0; i < tabCount; i++) {
+            result.append("    ");
+        }
     }
 }
--- Homework#2/src/main/java/it/sevenbits/main/Main.java
+++ Homework#2/src/main/java/it/sevenbits/main/Main.java
@@ -1,7 +1,37 @@
 package it.sevenbits.main;
 
+import it.sevenbits.formatter.Formatter;
+
 public class Main {
     public static void main(String[] args) {
+        Formatter formatter = new Formatter();
+        String test1 = "aaa {\n" +
+                "    bbbb;\n" +
+                "    ccc;\n" +
+                "}";
 
+        String test2 = "while   (inputStream.hasNext())  {\n" +
+                "   char symbol = inputStream.read();\n" +
+                "   if (symbol == ' ') {\n" +
+                "       whiteSpaceCount++;\n" +
+                "   }\n" +
+                "}";
+
+        String test3 = "while   (inputStream.hasNext())  {\n" +
+                "   char symbol = inputStream.read();\n" +
+                "   if (symbol == ' ') {\n" +
+                "       whiteSpaceCount++;\n" +
+                "       if (symbol == ' ') {\n" +
+                "           whiteSpaceCount++;\n" +
+                "           whiteSpaceCount++;\n" +
+                "           whiteSpaceCount++;\n" +
+                "       }\n" +
+                "   }\n" +
+                "}";
+        System.out.println(formatter.format(test1));
+        formatter = new Formatter();
+        System.out.println(formatter.format(test2));
+        formatter = new Formatter();
+        System.out.println(formatter.format(test3));
     }
 }
